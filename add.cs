0053d78890d811c8b951e9f02162be55a9dbe060
poornima-muru <135083818+poornima-muru@users.noreply.github.com>
2023-06-03 14:09:35 +0000

    sampleProgram

---
+++ add.cs
@@ -0,0 +1,8 @@
+using system;
+namespace program
+{
+    public static void main(string[] args)
+    {
+        Console.WriteLine("hello!");
+    }
+}
---
+++ sample.cs
@@ -0,0 +1,8 @@
+using System;
+namespace sample
+ {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+ }
