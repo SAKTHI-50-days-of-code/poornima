67d26c41c32dfc094fb983466b38c86a3bb96a8e
poornima-muru <135083818+poornima-muru@users.noreply.github.com>
2023-06-25 10:59:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Add_Two_numbers
+{
+    class Add
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            float y = 5;
+            var z = x + y;
+            Console.WriteLine(z);
+
+        }
+    }
+        
+}
